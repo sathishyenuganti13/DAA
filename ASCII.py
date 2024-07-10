4535c1195e7979fc8dbaeb9042e587aec542a501
sathishyenuganti13 <116738309+sathishyenuganti13@users.noreply.github.com>
2024-07-10 15:22:39 +0530

    Create ASCII.py

---
+++ ASCII.py
@@ -0,0 +1,19 @@
+string = input("enter the string : ")
+l = 1
+dic={}
+
+for i in range(97,123):
+    alpha = chr(i)
+    dic.update({alpha:l})
+    l+=1
+    
+flag = True
+
+for i in string:
+    if (string.count(i)!=dic[i]):
+        flag = False
+        break
+    else:
+        continue
+
+print("Yes") if (flag) else  print("No")
