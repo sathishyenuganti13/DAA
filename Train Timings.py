9a646c97f437e343d0a13e20cdaa9d2543018609
sathishyenuganti13 <116738309+sathishyenuganti13@users.noreply.github.com>
2024-07-11 07:11:41 +0530

    Update Train Timings.py

--- Train Timings.py
+++ Train Timings.py
@@ -1,17 +1,24 @@
-import math
-time = 19.34
-l=[4,9,15,24,35]
-for i in range(len(l)):
-    l[i]=(l[i]/60)*100
-hrs, mins = str(time).split(".")
+time = float(input("Enter the train start time: "))
 
-mins=(int(mins)/60)*100
+hrs, mins = str(time).split(".")
 hrs = int(hrs)
-result = []
-for i in l:
-    minutes= (mins+i)
-    hr= minutes//100
-    hr+=hrs
-    hr%=24
-    minutes = round(((minutes%100)*60)/100,2)/100
-    print(hr+minutes)
+mins = int(mins)
+
+if time >= 0 and hrs in range(24) and mins in range(60):
+    addMinutes = [4, 9, 15, 24, 35]
+    for i in range(len(addMinutes)):
+        addMinutes[i] = (addMinutes[i] / 60) * 100
+        
+    mins = mins / 60 * 100
+    
+    for i in addMinutes:
+        minutes = (mins + i)
+        hr = (minutes // 100 + hrs) % 24
+        minutes = round(((minutes % 100) * 60) / 100, 2) / 100
+        res=str(hr+minutes)
+        if hr<10: res='0'+res
+        if len(res)<5: res+='0'
+        print(res)
+        
+else:
+    print("Invalid Input")
